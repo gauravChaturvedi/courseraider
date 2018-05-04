0316766a2ba64849ce700a9fda4347acee27b8a9
gauravchaturvedi <gkchat10@gmail.com>
2018-05-04 02:25:04 -0400

    Removed some useless code

--- src_users/components/FormLink.js
+++ src_users/components/FormLink.js
@@ -8,30 +8,9 @@ import BarHistoChart from './BarHistoChart';
 // Dashboard component
 export class Dashboard extends React.Component {
 
-  constructor(props) {
-    super(props);
-    // default ui local state
-    this.state = {
-      QRCode: null
-    };
-  }
-
-  componentWillMount() {
-    const bitlyLink = localStorage.getItem('bitlyLink');
-    console.log(bitlyLink);
-    axios.get('http://ec2-52-70-176-147.compute-1.amazonaws.com:5000/api/v1/questions/qrcode?url=bit.ly/2KA2em3')
-      .then(res => {
-        // console.log(res.data);
-        this.setState({
-          QRCode: res.data
-        });
-      })
-  }
-
   // render
   render() {
     const bitlyLink = localStorage.getItem('bitlyLink');
-
 
     return (
       <div className="">
