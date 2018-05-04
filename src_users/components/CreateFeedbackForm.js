f851616199f4181162fdff8cd78b93054c757159
gauravchaturvedi <gkchat10@gmail.com>
2018-05-04 00:34:21 -0400

    Form Saving API integrated // Still need to change class ID bit

--- src_users/components/CreateFeedbackForm.js
+++ src_users/components/CreateFeedbackForm.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
+import axios from 'axios';
 import _ from 'lodash';
 import DynamicFormElement from "./common/DynamicFormElement.js";
 import Alert from 'react-s-alert';
 
 
 // CreateFeedbackForm component
-export default class CreateFeedbackForm extends React.Component {
+export class CreateFeedbackForm extends React.Component {
   constructor(props) {
     super(props);
 
@@ -15,10 +17,42 @@ export default class CreateFeedbackForm extends React.Component {
       description: '',
       type: "object",
       required: [],
-      properties: {},
-      elements: [],
+      properties: {
+           "Question1": {
+               "type": "string",
+               "title": "Question 1",
+               "enum": ["1", "2", "3", "4", "5"]
+           },
+          "Question2": {
+               "type": "string",
+               "title": "Question 2",
+               "enum": ["1", "2", "3", "4", "5"]
+          },
+          "Question3": {
+               "type": "string",
+               "title": "Question 3",
+               "enum": ["1", "2", "3", "4", "5"]
+          },
+          "Question4": {
+               "type": "string",
+               "title": "Question 4",
+               "enum": ["1", "2", "3", "4", "5"]
+          },
+          "Question5": {
+               "type": "string",
+               "title": "Feedback"
+          }
+      },
+      elements: [
+        {"id": "Question1", "radio": true, "type": "string", "label":"Difficulty", "options": "1,2,3,4,5"},
+        {"id": "Question2", "radio": true, "type": "string", "label":"Interest", "options": "1,2,3,4,5"},
+        {"id": "Question3", "radio": true, "type": "string", "label":"Helpfulness", "options": "1,2,3,4,5"},
+        {"id": "Question4", "radio": true, "type": "string", "label":"Satisfaction", "options": "1,2,3,4,5"},
+        {"id": "Question5", "radio": false, "type": "string", "label":"Feedback"}
+      ],
       UISchema: {},
-      ctr: 0
+      ctr: 5,
+      currentNoOfClasses: props.performance && props.performance.data && props.performance.data.length
     };
 
     // bind <this> to the event method
@@ -29,12 +63,12 @@ export default class CreateFeedbackForm extends React.Component {
     this.saveForm = this.saveForm.bind(this);
     this.generateUISchema = this.generateUISchema.bind(this);
   }
+
   // render
   render() {
-    // console.log(this.state.properties, 'new properties');
     const formElements = [];
     this.state.elements.forEach((elem, i) => {
-      formElements.push(<DynamicFormElement key={i} id={elem.id} radio={elem.radio} removeElement={this.removeElement} labelUpdate={this.labelUpdate} optionsUpdate={this.optionsUpdate} type={elem.type} />);
+      formElements.push(<DynamicFormElement key={i} id={elem.id} radio={elem.radio} removeElement={this.removeElement} labelUpdate={this.labelUpdate} optionsUpdate={this.optionsUpdate} type={elem.type} label={elem.label} options={elem.options} />);
     });
     return (
       <div className="page-home">
@@ -62,7 +96,7 @@ export default class CreateFeedbackForm extends React.Component {
   }
 
   addElement(type, radioFlag) {
-    const elemId = type + this.state.ctr;
+    const elemId = 'Question' + (this.state.ctr + 1);
     const newObj = {
       type: type,
       id: elemId,
@@ -141,6 +175,18 @@ export default class CreateFeedbackForm extends React.Component {
         type: this.state.type
       }
 
+      const postData = {
+        'class_id': 1990,
+        'ui_schema': UISchema,
+        'form_schema': formSchema
+      };
+
+      axios.post('http://ec2-52-70-176-147.compute-1.amazonaws.com:5000/api/v1/questions', postData)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
+
       this.setState({
         UISchema: UISchema
       });
@@ -159,3 +205,10 @@ export default class CreateFeedbackForm extends React.Component {
   }
 
 }
+
+function mapStateToProps(state) {
+  return {
+    performance: state.performance
+  };
+}
+export default connect(mapStateToProps)(CreateFeedbackForm);
--- src_users/components/common/DynamicFormElement.js
+++ src_users/components/common/DynamicFormElement.js
@@ -15,14 +15,26 @@ export default class DynamicFormElement extends React.Component {
     // this.removeElement = this.props.removeElement.bind(this);
   }
 
+  componentWillMount() {
+    const { label, labelUpdate, options, optionsUpdate, id } = this.props;
+    if (label) {
+        labelUpdate(id, label);
+    }
+
+    if (options) {
+        optionsUpdate(id, options);
+    }
+  }
+
 
 // render
   render() {
-    const { type, radio, id, removeElement, labelUpdate, optionsUpdate } = this.props;
+    const { type, radio, id, removeElement, labelUpdate, optionsUpdate, label, options } = this.props;
+
     return (
       <div className="dynamic-element-container">
-        <label>Question Text:</label> <input className="form-control question-label-input" onBlur={(evt)=> labelUpdate(id, evt.target.value)}/> <span className="remove-button" onClick={() => removeElement(id)}>Remove</span>
-        { !radio ? '' : <div> <label>Options:</label> <input className="form-control question-label-input" onBlur={(evt)=> optionsUpdate(id, evt.target.value)} /> (separate by commas) </div>}
+        <label>Question Text:</label> <input className="form-control question-label-input" defaultValue={label || ''} onBlur={(evt)=> labelUpdate(id, evt.target.value)}/> <span className="remove-button" onClick={() => removeElement(id)}>Remove</span>
+        { !radio ? '' : <div> <label>Options:</label> <input className="form-control question-label-input" defaultValue={options || ''} onBlur={(evt)=> optionsUpdate(id, evt.target.value)} /> (separate by commas) </div>}
       </div>
     );
   }
